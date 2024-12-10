951cbe220d8780b8cf41739fba24010944104e6a
alex <alex.henrique01@hotmail.com>
2024-12-09 22:59:04 -0300

    primeiro commit

---
+++ rolagem_de_pagina.js
@@ -0,0 +1,17 @@
+// script.js
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+  
+      const target = document.querySelector(this.getAttribute('href'));
+  
+      if (target) {
+        target.scrollIntoView({
+          behavior: 'smooth', // Rolagem suave
+          block: 'start'      // Alinha no início da seção
+        });
+      }
+    });
+  });
+  
